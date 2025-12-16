29c87881cdddc3f7823a3bd1163982315354acf5
sidewaysdesign <richard@sidewaysdesign.com>
2025-12-16 10:57:14 -0500

    Updated all ws-script packages

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '2c01bf2bd09f01854f14');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'f9530e0733602d3ea19f');
